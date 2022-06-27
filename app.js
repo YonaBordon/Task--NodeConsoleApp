d78301733a1c353b59ba5625614867ac0dfb6d8c
Shonakar <carlosjbordon@gmail.com>
2022-06-27 15:24:49 -0300

    Create Task list and model.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const { inqMenu, inqPause } = require('./helpers/inquirerMenu');
+const { Task } = require('./models/task');
+const Tasks = require('./models/Tasks');
 
 console.clear();
 
 const main = async () => {
+  let option = '';
   do {
     option = await inqMenu();
     console.log({ option });
+
     if (option !== '0') await inqPause();
   } while (option !== '0');
 };
---
+++ models/Tasks.js
@@ -0,0 +1,9 @@
+// {uuid: { id: , desc: }}
+class Tasks {
+  _list = {};
+
+  constructor() {
+    this._list = {};
+  }
+}
+module.exports = Tasks;
